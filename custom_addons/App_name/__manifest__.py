f4523af824fef72651e1f2614ce11b8ddb3b4d96
Dharmik <patel.dharmik.harshadkumar@gmail.com>
2024-10-10 10:30:06 +0530

    'test'

--- custom_addons/App_name/__manifest__.py
+++ custom_addons/App_name/__manifest__.py
@@ -1,4 +1,9 @@
 {
     "name":"First_app",
     'author':'Dharmik',
+    'license':'LGPL-3',
+    'data': [
+        'views/Users_view.xml',
+        'views/view.xml'
+    ]
 }
--- custom_addons/App_name/models/model_name.py
+++ custom_addons/App_name/models/model_name.py
@@ -3,3 +3,8 @@ from odoo import fields, models
 class First(models.Model):
     _name = 'first.module'
     _description='Hello World,That is My First Module'
+
+    name=fields.Char(string='Name',required=True)
+    DoB=fields.Date(string="Date Of Birth",required=True)
+    gender=fields.Selection([('male','Male'),('female','Female')],string='Gender',required=True)
+    age=fields.Integer('Age',required=True)
